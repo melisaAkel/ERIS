d57e0ee5f66f3a582084b69cb52256caa5ddb1a1
ilsukaradag <ilsukaradag12@gmail.com>
2025-07-20 21:11:21 +0300

    Modified main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from fastapi.middleware.cors import CORSMiddleware
@@ -210,21 +209,3 @@ def get_route(req: RouteRequest):
     except Exception as e:
         print(f"GraphHopper error: {str(e)}")
         return {"error": str(e)}
-=======
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
->>>>>>> 468c704c9194232fb7736b1f4a9b051ebc18ebf9
